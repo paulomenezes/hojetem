1cad6df6f388d85f01a56502279aeca44a17b4a4
Paulo Menezes <paulo.hgmenezes@gmail.com>
2017-04-18 11:11:16 -0300

    Remove modules and web

--- web/adm/logoff.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-
-	$_SESSION['store'] = '';
-	$_SESSION['adm'] = '';
-
-	header("Location:index.php");
-?>
--- web/js/AboutController.js
+++
@@ -1,3 +0,0 @@
-app.controller('AboutController', function ($scope, $http, $mdDialog, $routeParams, $location) {
-	
-});
